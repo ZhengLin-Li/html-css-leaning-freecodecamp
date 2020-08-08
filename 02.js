e3db15b44371db6beb034aeb05c35856e9692c43
ZhengLin-Li <63448884+ZhengLin-Li@users.noreply.github.com>
2020-08-09 06:50:34 +0800

    2020/8/9

---
+++ 02.js
@@ -0,0 +1,37 @@
+// 初始化变量
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["JavaScript", "Gaming", "Foxes"]
+    }
+];
+
+
+function lookUpProfile(name, prop){
+// 请把你的代码写在这条注释以下
+
+// 请把你的代码写在这条注释以上
+}
+
+// 你可以修改这一行来测试你的代码
+lookUpProfile("Akira", "likes");
